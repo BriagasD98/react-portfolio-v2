d7d3dcdd809931d1273904feb3bac45f88893acb
BriagasD98 <briagasdavid@yahoo.com>
2022-07-01 22:01:03 -0500

    errorpage component and link added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
-import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import Home from './Pages/Home';
 import About from './Pages/About';
 import Portfolio from './Pages/Portfolio';
+import ErrorPage from './Pages/ErrorPage';
 
 function App() {
   return (
     <Router>
+      <nav>
+        <Link to='/'> Home </Link>
+        <Link to='/about'> About </Link>
+        <Link to='/portfolio'> Portfolio </Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />        
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
---
+++ src/Pages/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ErrorPage() {
+  return (
+    <div>This is the ErrorPage! Very Strange indeed...</div>
+  )
+}
+
+export default ErrorPage
