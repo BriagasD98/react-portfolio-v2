af65b7a6eda594af8d1b98f5db0b416b48a07493
BriagasD98 <briagasdavid@yahoo.com>
2022-07-04 12:07:46 -0500

    css updates for about component

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import David from '../../Assets/Images/davidbheadshot.JPG'
 
 function About() {
   return (
-      <div className='container'>THIS IS THE ABOUT COMPONENT</div>
+        <div className='container2'>
+
+          <div className='card'>
+            <img className='david' src={David} alt="David" />
+          </div>
+
+          <div className='card'>
+            <p>I'm a Full-Stack Developer leveraging a strong background
+               in entrepreneurship to put 110% into everything I do and creative thinking
+                to find success. Recently earned a certificate in Full-Stack Web Development
+                 through The University of Texas at Austin. Newly developed skills in
+                  the MERN stack such as React, MongoDB, Express and Node. Skills to make
+                   a visually appealing & responsive website include CSS, Bootstrap, Bulma and Handlebars.
+                    Known as a creative problem-solver with a passion for developing sleek & intuitive
+                     web applications. With each project, my goal is to keep my target audience
+                      engaged and create recurring users on the site. I have worked effectively on a team
+                       multiple times to deliver a project by specific deadlines. I’m excited to leverage
+                        my skills in the industry to develop cutting-edge applications as part
+                         of a tenacious & enthusiastic team.</p>
+          </div>
+
+        </div>
   )
 }
 
