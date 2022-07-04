33098443b1eab34a2cc397a7890d1c263d6ca08b
BriagasD98 <briagasdavid@yahoo.com>
2022-07-04 17:06:27 -0500

    flex boxes added to portfolio component

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -4,12 +4,17 @@ import { motion } from 'framer-motion'
 
 function Portfolio() {
   return (
-    <motion.div className='container'
+    <motion.div className='container2'
     initial={{width: 0}}
     animate={{width: "100vw"}}
     exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
     >
-      THIS IS THE PORTFOLIO COMPONENT
+      <div className='card2 box1'></div>
+      <div className='card2 box2'></div>
+      <div className='card2 box3'></div>
+      <div className='card2 box4'></div>
+      <div className='card2 box5'></div>
+      <div className='card2 box6'></div>
     </motion.div>
   )
 }
