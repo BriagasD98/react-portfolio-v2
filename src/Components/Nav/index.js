a222619db362e56bdde247c63d56ae2c1323a96a
BriagasD98 <briagasdavid@yahoo.com>
2022-07-04 19:53:19 -0500

    portfolio card content progress

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -6,8 +6,8 @@ function Nav() {
     <nav style={{ width: "100%" }}>
         {/* <Link to='/'> About </Link> */}
         <Link to='/about'> About </Link>
-        <Link to='/contact'> Contact </Link>
         <Link to='/portfolio'> Portfolio </Link>
+        <Link to='/contact'> Contact </Link>
         <Link to='/resume'>Resume</Link>
       </nav>
   )
--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ghIcon from '../../Assets/Images/gh-icon2.png'
 
 import { motion } from 'framer-motion'
 
@@ -9,8 +10,24 @@ function Portfolio() {
     animate={{width: "100vw"}}
     exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
     >
-      <div className='card2 box1'>Box 1</div>
-      <div className='card2 box2'>Box 2</div>
+      <div className='card2 box1'>Box 1
+        <div className='cardtitle'>
+          <h2>Project 1</h2>
+          <h2>Example 1</h2>
+        </div>
+        <div className='btnboard'>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+        </div>
+      </div>
+      <div className='card2 box2'>Box 2
+      <a>Project 2</a>
+      <span>Example2</span>
+      </div>
       <div className='card2 box3'>Box 3</div>
       <div className='card2 box4'>Box 4</div>
       <div className='card2 box5'>Box 5</div>
