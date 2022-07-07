807b91ed9da7f08d840db2b2b00e089ff20430a2
BriagasD98 <briagasdavid@yahoo.com>
2022-07-07 11:38:17 -0500

    all content cards populated with uniform setup

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -24,14 +24,81 @@ function Portfolio() {
           </a>
         </div>
       </div>
-      <div className='card2 box2'>Box 2
-      <a>Project 2</a>
-      <span>Example2</span>
+
+      <div className='card2 box2'>
+        <div className='cardtitle'>
+          <h2>Project 1</h2>
+          <h2>Example 1</h2>
+        </div>
+        <div className='btnboard'>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+        </div>
       </div>
-      <div className='card2 box3'>Box 3</div>
-      <div className='card2 box4'>Box 4</div>
-      <div className='card2 box5'>Box 5</div>
-      <div className='card2 box6'>Box 6</div>
+
+      <div className='card2 box3'>
+        <div className='cardtitle'>
+          <h2>Project 1</h2>
+          <h2>Example 1</h2>
+        </div>
+        <div className='btnboard'>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+        </div>
+      </div>
+
+      <div className='card2 box4'>
+        <div className='cardtitle'>
+          <h2>Project 1</h2>
+          <h2>Example 1</h2>
+        </div>
+        <div className='btnboard'>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+        </div>
+      </div>
+
+      <div className='card2 box5'>
+        <div className='cardtitle'>
+          <h2>Project 1</h2>
+          <h2>Example 1</h2>
+        </div>
+        <div className='btnboard'>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+        </div>
+      </div>
+
+      <div className='card2 box6'>
+        <div className='cardtitle'>
+          <h2>Project 1</h2>
+          <h2>Example 1</h2>
+        </div>
+        <div className='btnboard'>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+          <a href="https://github.com/BriagasD98">
+            <img className='icon2' src={ghIcon} alt="Github" />
+          </a>
+        </div>
+      </div>
     </motion.div>
   )
 }
