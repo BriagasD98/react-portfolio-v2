81337ed73f39a8709909c35bb12d77e296847e8b
BriagasD98 <briagasdavid@yahoo.com>
2022-07-10 18:49:15 -0500

    portfolio card titles updated

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -12,8 +12,8 @@ function Portfolio() {
     >
       <div className='card2 box1'>
         <div className='cardtitle'>
-          <h2>Project 1</h2>
-          <h2>Example 1</h2>
+          <h2>CherryPicker</h2>
+          <h2>React, APIs & CSS</h2>
         </div>
         <div className='btnboard'>
           <a href="https://github.com/BriagasD98">
@@ -27,8 +27,8 @@ function Portfolio() {
 
       <div className='card2 box2'>
         <div className='cardtitle'>
-          <h2>Project 2</h2>
-          <h2>Example 2</h2>
+          <h2>Gopher 2.0</h2>
+          <h2>React, SQL, APIs & CSS</h2>
         </div>
         <div className='btnboard'>
           <a href="https://github.com/BriagasD98">
@@ -42,8 +42,8 @@ function Portfolio() {
 
       <div className='card2 box3'>
         <div className='cardtitle'>
-          <h2>Project 3</h2>
-          <h2>Example 3</h2>
+          <h2>Gopher 1.0</h2>
+          <h2>HTML, CSS & APIs</h2>
         </div>
         <div className='btnboard'>
           <a href="https://github.com/BriagasD98">
@@ -57,8 +57,8 @@ function Portfolio() {
 
       <div className='card2 box4'>
         <div className='cardtitle'>
-          <h2>Project 4</h2>
-          <h2>Example 4</h2>
+          <h2>Social API</h2>
+          <h2>MONGO DB & APIs</h2>
         </div>
         <div className='btnboard'>
           <a href="https://github.com/BriagasD98">
@@ -72,8 +72,8 @@ function Portfolio() {
 
       <div className='card2 box5'>
         <div className='cardtitle'>
-          <h2>Project 5</h2>
-          <h2>Example 5</h2>
+          <h2>Tech Blog</h2>
+          <h2>SQL, HTML & CSS</h2>
         </div>
         <div className='btnboard'>
           <a href="https://github.com/BriagasD98">
@@ -87,8 +87,8 @@ function Portfolio() {
 
       <div className='card2 box6'>
         <div className='cardtitle'>
-          <h2>Project 6</h2>
-          <h2>Example 6</h2>
+          <h2>Run Buddy</h2>
+          <h2>HTML & CSS</h2>
         </div>
         <div className='btnboard'>
           <a href="https://github.com/BriagasD98">
