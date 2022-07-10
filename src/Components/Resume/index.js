0876c59d08b036ec3314074c282bda760213451b
BriagasD98 <briagasdavid@yahoo.com>
2022-07-09 20:10:23 -0500

    download resume icon added

--- src/Components/Resume/index.js
+++ src/Components/Resume/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 import liQRCode from '../../Assets/Images/DavidB-LI-QR.png'
+import gdocs from '../../Assets/Images/google-docs.png'
 
 
 function Resume() {
@@ -14,17 +15,26 @@ function Resume() {
     <div className='container3'>
 
       <div className='card3 resume'>
-        <div>
-          <h2>Click Here to View Resume!</h2>
+        {/* <div>
+          <h2>Click <span>HERE</span> to View Resume!</h2>
         </div>
         <div>
-          <h2>Download My Resume Here!</h2>
-        </div>
+          <h2>Download My Resume <span>HERE</span>!</h2>
+        </div> */}
+        <nav className='iconlist'>
+          <a>
+          <h2>Click <span>HERE</span> to View Resume!</h2>
+          <img className='icon' src={gdocs} />
+          </a>
+          <a>
+            <h2>Download My Resume <span>HERE</span>!</h2>
+          </a>
+        </nav>
       </div>
 
       <div className='card3 resume'>
         <div>
-          <h2>SCAN ME To View Portfolio App On Mobile Device!</h2>
+          <h2><span>SCAN ME</span> To View Portfolio App On Mobile Device!</h2>
           <img className='icon3' src={liQRCode} />
         </div>
       </div>
