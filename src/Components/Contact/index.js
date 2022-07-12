f4201b66227333f2f2669e44038f7e649ef6efd2
BriagasD98 <briagasdavid@yahoo.com>
2022-07-11 22:16:58 -0500

    emailjs installed

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -1,14 +1,25 @@
 import React from "react";
+import emailjs from 'emailjs-com'
 import { motion } from 'framer-motion'
 
 function Contact() {
+
+    function sendEmail(e) {
+
+    }
+
     return (
         <motion.div className='container'
         initial={{width: 0}}
         animate={{width: "100vw"}}
         exit={{ x: window.innerWidth, transition: { duration: 0.1 } }}
         >
-            THIS IS THE CONTACT COMPONENT
+            <div className="container">
+                <form onSubmit={sendEmail}>
+                    
+                </form>
+            </div>
+
         </motion.div>
     )
 }
