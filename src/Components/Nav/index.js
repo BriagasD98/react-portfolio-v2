2f02eb745eed48d7dbd44a125f2f1d742fb1b18f
BriagasD98 <briagasdavid@yahoo.com>
2022-07-12 23:57:24 -0500

    responsive navbar started, button appears with media query

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -3,13 +3,16 @@ import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <nav style={{ width: "100%" }}>
+    <div className='renav'>
+      <nav style={{ width: "100%" }}>
         {/* <Link to='/'> About </Link> */}
         <Link to='/about'> About </Link>
         <Link to='/portfolio'> Portfolio </Link>
         <Link to='/contact'> Contact </Link>
         <Link to='/resume'>Resume</Link>
       </nav>
+      <button>Open</button>
+    </div>
   )
 }
 
