83e0385fdcd5e78fb061bd27a8b1f8154088cd79
BriagasD98 <briagasdavid@yahoo.com>
2022-07-15 23:28:31 -0500

    hashrouter tested and working as it should

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom'
 import Nav from './Components/Nav';
-import Footer from './Components/Footer';
 import AnimatedRoutes from './Components/AnimatedRoutes';
 
 function App() {
   return (
     <main>
       <div className='container navbar'>
-        {/* <h1 className='header'>DAVID BRIAGAS</h1> */}
       <Nav></Nav>
       </div>
       <AnimatedRoutes />
--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ import { HashRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter hashType="slash">
     <App />
   </HashRouter>,
   root
